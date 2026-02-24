23153772976df1c467e9aac064fd1fd8508fad09
atcsol <atcsol@atcsol>
2026-02-24 18:29:11 +0100

    commit

--- Pract1/Archivos C/Debug/llsw/leon3_uart_drv/src/leon3_uart_drv.d
+++
@@ -1,6 +0,0 @@
-llsw/leon3_uart_drv/src/leon3_uart_drv.o: \
- ../llsw/leon3_uart_drv/src/leon3_uart_drv.c \
- ../llsw/leon3_uart_drv/src/../../../llsw/leon3_uart_drv/include/public/leon3_uart_drv_v1.h \
- ../llsw/leon3_uart_drv/src/../../../llsw/leon3_uart_drv/include/public/../../../../llsw/config/include/public/basic_types.h
-../llsw/leon3_uart_drv/src/../../../llsw/leon3_uart_drv/include/public/leon3_uart_drv_v1.h:
-../llsw/leon3_uart_drv/src/../../../llsw/leon3_uart_drv/include/public/../../../../llsw/config/include/public/basic_types.h:
--- Pract1/Archivos C/Debug/service_libraries/edroomsl/edroomsl_types/src/edroomsl_types.d
+++
@@ -1,8 +0,0 @@
-service_libraries/edroomsl/edroomsl_types/src/edroomsl_types.o: \
- ../service_libraries/edroomsl/edroomsl_types/src/edroomsl_types.cpp \
- ../service_libraries/edroomsl/edroomsl_types/src/../../edroomsl_types/include/public/edroomsl_types.h \
- ../service_libraries/edroomsl/edroomsl_types/src/../../edroomsl_types/include/public/../../../edroomsl_types/include/edroomsl_types/edroomsl_types.h \
- ../service_libraries/edroomsl/edroomsl_types/src/../../edroomsl_types/include/public/../../../edroomsl_types/include/edroomsl_types/../../../../../llsw/config/include/public/basic_types.h
-../service_libraries/edroomsl/edroomsl_types/src/../../edroomsl_types/include/public/edroomsl_types.h:
-../service_libraries/edroomsl/edroomsl_types/src/../../edroomsl_types/include/public/../../../edroomsl_types/include/edroomsl_types/edroomsl_types.h:
-../service_libraries/edroomsl/edroomsl_types/src/../../edroomsl_types/include/public/../../../edroomsl_types/include/edroomsl_types/../../../../../llsw/config/include/public/basic_types.h:
